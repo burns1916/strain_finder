b5361ee93811014996f811db0ac6cc91b7f0906a
Ryan K Burns <ryankburns@gmail.com>
2020-10-26 04:28:50 +0000

    added find_by methods

--- lib/strain_finder/scraper.rb
+++ lib/strain_finder/scraper.rb
@@ -4,11 +4,34 @@ require 'httparty'
 
 class StrainFinder::Scraper
 
-  def self.fetch_strains
+  def self.fetch_all_strains
     url = 'http://strainapi.evanbusse.com/y4hQMu2/strains/search/all'
     resources = HTTParty.get(url)
     StrainFinder::Strains.new_from_collection(resources)
-    binding.pry
+  end
+
+  def self.fetch_strains_by_name(name)
+    url = 'http://strainapi.evanbusse.com/y4hQMu2/strains/search/name/{#name}'
+    resources = HTTParty.get(url)
+    StrainFinder::Strains.new_from_collection(resources)
+  end
+
+  def self.fetch_strains_by_race(race)
+    url = 'http://strainapi.evanbusse.com/y4hQMu2/strains/search/race/#{race}'
+    resources = HTTParty.get(url)
+    StrainFinder::Strains.new_from_collection(resources)
+  end
+
+  def self.fetch_strains_by_effect(effect)
+    url = 'http://strainapi.evanbusse.com/y4hQMu2/strains/search/effect/{#effect}'
+    resources = HTTParty.get(url)
+    StrainFinder::Strains.new_from_collection(resources)
+  end
+
+  def self.fetch_strains_by_flavor(flavor)
+    url = 'http://strainapi.evanbusse.com/y4hQMu2/strains/search/flavor/{#flavor}'
+    resources = HTTParty.get(url)
+    StrainFinder::Strains.new_from_collection(resources)
   end
 
 end
--- lib/strain_finder/strains.rb
+++ lib/strain_finder/strains.rb
@@ -1,6 +1,6 @@
 class StrainFinder::Strains
 
-  attr_accessor :id, :name, :race, :flavor, :effects, :medicinal
+  attr_accessor :id, :name, :race, :flavor, :effect
 
   @@all = []
 
@@ -17,8 +17,22 @@ class StrainFinder::Strains
     @@all << self
   end
 
+  def find_by_race(race)
+    all.find{|s| s.race == race}
+  end
+
+  def find_by_flavor(flavor)
+    all.find{|s| s.flavor == flavor}
+  end
+
+  def find_by_effect(effect)
+    all.find{|s| s.effect == effect}
+  end
+
   def self.new_from_collection(strains)
     strains.each {|attributes| self.new(attributes)}
   end
+
+
 
 end
